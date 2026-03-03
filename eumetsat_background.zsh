0fbd633cda78afbe56e235cf7b5e83a6251fa16f
JV <jaiveer.chadda@gmail.com>
2026-03-03 23:46:44 +0000

    Initial Commit

---
+++ eumetsat_background.zsh
@@ -0,0 +1,42 @@
+#!/usr/bin/env zsh
+
+
+eumetsat_bg::download_image() {
+
+  local _self_location="${CS}/x_Automation/Mac Background/EUMETSAT"
+  local _resources_dir="${_self_location}/resources"
+  local _target_file="${_resources_dir}/most_recent_img.png"
+
+
+  local domain='https://view.eumetsat.int/geoserver/ows'
+  
+  local service='WMS'
+  local request='GetMap'
+  local version='1.3.0'
+  
+  local -a layers_arr=( 'mtg_fd:rgb_geocolour' )
+  
+  local format='image/png'
+  local crs='EPSG:4326'
+  
+  local -a bounding_box=( -82 -82 82 82 )
+  
+  local -A _dims=( [width]=800 [height]=800 )
+  local dimensions="width=${_dims[width]}&height=${_dims[height]}"
+
+  local -a arguments=( 
+    "service=${service}"
+    "request=${request}"
+    "version=${version}"
+    "layers=${(j:,:)layers_arr}"
+    "format=${format}"
+    "crs=${crs}"
+    "bbox=${(j:,:)bounding_box}"
+    "${dimensions}"
+  )
+
+
+  curl "${domain}?${(j:&:)arguments}" > "${_target_file}"
+
+}
+
